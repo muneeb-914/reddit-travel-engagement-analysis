c95557c1a922b5dfcf76e05edab70e2b2dfaefe6
muneeb-914 <mu181842@example.com>
2026-01-23 23:35:31 +0500

    Working on Reddit API logic - not finished

--- scripts/ana.py
+++ scripts/ana.py
@@ -2,21 +2,32 @@ import pandas as pd
 from datetime import datetime
 df = pd.read_csv('data/raw_reddit_data.csv')
 
+# ==================================================================================
+# Which posts get highest engagement?
+
 df ['engagement'] = df['score'] + df['num_comments']
-# top_engagement_posts = df.sort_values(
-#     by="engagement", ascending=False
-# ).head(10)
+top_engagement_posts = df.sort_values(
+    by="engagement", ascending=False
+).head(10)
 
-# print(top_engagement_posts[["title", "score", "num_comments", "engagement"]])
+print(top_engagement_posts[["title", "score", "num_comments", "engagement"]])
 
+# ==================================================================================
 
-# df[["score", "num_comments"]].corr()
+# Do posts with more comments also have higher scores?
 
-# print(df[["score", "num_comments"]].corr())
+print(df[["score", "num_comments"]].corr())
+
+# ==================================================================================
+
+# Top 10 most engaging posts
+
 
 df ["created_utc"] = pd.to_datetime(df['created_utc'])
 df ["hours"] = df["created_utc"].dt.hour
 df ["days_of_week"] = df["created_utc"].dt.day_name()
 
 
-print(df.groupby("hours")["engagement"].mean().sort_index(ascending=False))
+print(df.groupby("hours")["engagement"].mean().sort_values(ascending=False))
+
+print(df.groupby("days_of_week")["engagement"].mean().sort_values(ascending=False))
